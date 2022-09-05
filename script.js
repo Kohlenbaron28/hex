1495de71c729ac1e77beb98c8f16de3fb1562d41
Kohlenbaron28 <110342046+Kohlenbaron28@users.noreply.github.com>
2022-09-05 23:13:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    let hex_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexcode= "";
+    for (let i=0; i<6; i++) {
+        /*let random_index=Math.floor(Math.random()*hex_numbers.length);*/
+        console.log(Math.random() * hex_numbers.length);
+    }
+}
